0380cc97e478972fa789e7427287eb3425dce8ce
JosephWitten <51753861+JosephWitten@users.noreply.github.com>
2020-05-19 06:25:03 -0400

    Add files via upload

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-
+//make sure html has correct onresize thingy in real doc
 const canvas = document.getElementById("background");
 const ctx = canvas.getContext("2d");
 const squares = 20
@@ -10,7 +10,7 @@ let map = []
 for (let i = 0; i < squares; i++) {
     map.push([])
     for (let j = 0; j < squares; j++) {
-        map[i].push(0)
+        map[i].push(squares-j+i)
     }
 }
 let squareWidth = width/squares
@@ -18,22 +18,20 @@ let squareHeight = height/squares
 
 
 window.addEventListener('resize', () => {
-    canvas.width = canvas.parentElement.clientWidth;
-    canvas.height = canvas.parentElement.clientHeight;
-    squareWidth = width/squares
-    squareHeight = height/squares
-    width = canvas.clientWidth;
-    height = canvas.clientHeight;
-    draw();
+    sizeChange()
+});
+
+window.addEventListener('fullscreenchange', () => {
+    sizeChange()
 });
 
 canvas.width = width
 canvas.height = height
     
-draw()
-console.log(width)
-console.log(height)
 
+draw()
+
+console.log(map)
 
 function draw() {
 
@@ -50,4 +48,18 @@ function draw() {
         }
         
     }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function sizeChange() {
+    canvas.width = canvas.parentElement.clientWidth;
+    canvas.height = canvas.parentElement.clientHeight;
+    squareWidth = width/squares
+    squareHeight = height/squares
+    width = canvas.clientWidth;
+    height = canvas.clientHeight;
+    draw();
 }
