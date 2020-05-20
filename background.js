e10beab5c222d91827947fb7750493ce387600cf
JosephWitten <51753861+JosephWitten@users.noreply.github.com>
2020-05-20 05:11:51 -0400

    undid alot, changed direction of the project, now tetris

--- background.js
+++ background.js
@@ -1,25 +1,28 @@
 //make sure html has correct onresize thingy in real doc
 const canvas = document.getElementById("background");
 const ctx = canvas.getContext("2d");
-const squares = 20
+const horizSquares = 10
+const vertSquares = 20
+const blockArray = ["longBlock", "lBlock", "cubeBlock", "tBlock", "zBlock"]
+
 
     
 let width = canvas.clientWidth;
 let height = canvas.clientHeight;
 
 let map = []
-for (let i = 0; i < squares; i++) {
+for (let i = 0; i < horizSquares; i++) {
     map.push([])
-    for (let j = 0; j < squares; j++) {
-        map[i].push(squares-j+i)
+    for (let j = 0; j < vertSquares; j++) {
+        map[i].push(0)
     }
 }
 
-const maximum = Math.max.apply(Math, map[squares-1])
-console.log(maximum)
 
-let squareWidth = width/squares
-let squareHeight = height/squares
+let blockInMotion = false
+
+let squareWidth = width/horizSquares
+let squareHeight = height/vertSquares
 
 
 window.addEventListener('resize', () => {
@@ -32,37 +35,44 @@ window.addEventListener('fullscreenchange', () => {
 
 canvas.width = width
 canvas.height = height
-console.log(map)
+
 draw()
 
 window.onload = async function() {
     while(true) {
-    let iterator = 0
-    while (iterator < maximum) {
+
+
         //becomes green + grey lines
         this.draw()
-        // one number of cells become blue
-        this.pulse(iterator)
+
+        if (!blockInMotion) {
+        this.createBlock()
+        }
+
+       
         //pause
-        await sleep(20)
+        await sleep(100)
 
         //full white
         this.clear()
-        iterator++;
+
+        this.fall()
+
+        
     }
-}
 }
 
 
 
 function draw() {
     
-    for (let i = 0; i < squares; i++) {
+    for (let i = 0; i < horizSquares; i++) {
         //console.log(i)
-        for (let j = 0; j < squares; j++) {
-       
-            ctx.fillStyle = "green"
+        for (let j = 0; j < vertSquares; j++) {
+            if(map[i][j] == 0) {
+            ctx.fillStyle = "#aaaaaa"
             ctx.fillRect(i*squareWidth, j*squareHeight, squareWidth, squareHeight)
+            }
 
             ctx.beginPath();
             ctx.strokeStyle = "grey"
@@ -74,20 +84,7 @@ function draw() {
    
 }
 
-function pulse(iterator) {
-    sleep(10)
-        
-        for (let i = 0; i < squares; i++) {
-            for (let j = 0; j < squares; j++) {
-         
-                if (map[i][j] == iterator) {
-                    ctx.fillStyle = "blue"
-                    ctx.fillRect(i*squareWidth, j*squareHeight, squareWidth, squareHeight)
-                }
-       
-            }
-        }
-    }
+
 
 
 function sleep(ms) {
@@ -100,9 +97,67 @@ function clear() {
 function sizeChange() {
     canvas.width = canvas.parentElement.clientWidth;
     canvas.height = canvas.parentElement.clientHeight;
-    squareWidth = width/squares
-    squareHeight = height/squares
+    squareWidth = width/horizSquares
+    squareHeight = height/vertSquares
     width = canvas.clientWidth;
     height = canvas.clientHeight;
     draw();
+}
+
+function randomPos() {
+    return Math.floor(Math.random() * Math.floor(horizSquares));
+}
+
+function randomBlock() {
+    return blockArray[Math.floor(Math.random()*blockArray.length)];
+}
+
+function createBlock() {
+    
+    let startPos = randomPos() 
+    let currentBlock = randomBlock()
+    if (currentBlock == "longBlock") {
+    map[startPos][0] =  1
+    map[startPos][1] = 1
+    map[startPos][2] = 1
+    map[startPos][3] = 1
+    console.log(map)
+    blockInMotion = true
+    }
+    
+    
+}
+
+function fall() {
+
+    let tempPosArray = []
+
+    for (let i = 0; i < horizSquares; i++) {
+        for (let j = 0; j < vertSquares; j++) {
+         
+            if (map[i][j] == 1) {
+                
+                tempPosArray.push([i, j])
+                
+
+
+            }
+           
+        }
+    }
+
+    
+    for (let i = 0; i < horizSquares; i++) {
+        for (let j = 0; j < vertSquares; j++) {
+         
+            for (let x = 0; x < tempPosArray.length; x++) {
+                map[tempPosArray[x][0]][tempPosArray[x][1]] = 0
+                }
+
+            for (let x = 0; x < tempPosArray.length; x++) {
+                map[tempPosArray[x][0]][tempPosArray[x][1] + 1] = 1
+                }
+        }
+    }
+
 }
